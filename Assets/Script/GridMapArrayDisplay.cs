b5540ed4826b456343810b975a3c1dee02010c21
Issac <gundam.issacwai@gmail.com>
2021-11-16 14:56:14 +0000

    Tile Implementation
    Completed

--- Assets/Script/GridMapArrayDisplay.cs
+++ Assets/Script/GridMapArrayDisplay.cs
@@ -10,16 +10,17 @@ public class GridMapArrayDisplay : MonoBehaviour
 
         for(int i = 0; i < 50; i++)
         {
-            for(int j = 0; i < 50; i++)
+            for(int j = 0; j < 50; j++)
             {
-                TileSpawn(i, 1, j);
+                TileSpawn(i, j);
             }
         }
     }
 
-    private void TileSpawn(int x, int y, int z)
+    private void TileSpawn(int x, int y)
     {
-        GameObject Map = new GameObject("X: "+x+"Y: "+y);
-        Map.transform.position = new Vector3(24.5f + x, 1, -24.5f + y);
+        GameObject Map = new GameObject("X: "+x+" | Y: "+y);
+        Map.transform.SetParent(gameObject.transform);
+        Map.transform.position = new Vector3(-24.5f + x, 1, -24.5f + y);
     }
 }
