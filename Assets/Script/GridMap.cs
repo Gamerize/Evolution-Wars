c6f6694dccba8e8b9b5d88f1ccccd470c8b0f9b1
Issac <gundam.issacwai@gmail.com>
2021-12-16 17:59:25 +0000

    Added UI

--- Assets/Script/GridMap.cs
+++ Assets/Script/GridMap.cs
@@ -6,16 +6,24 @@ public class GridMap : MonoBehaviour
 {
     private float length;
     private float width;
+    private float cellsize;
     private float[,] gridMapArray;
 
-    public GridMap(int width, int length)
+    public GridMap(int width, int length, float cellsize)
     {
         this.width = width;
         this.length = length;
+        this.cellsize = cellsize;
 
         gridMapArray = new float[width, length];
 
-        Debug.Log(width + " " + length);
+        for(int x = 0; x < gridMapArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridMapArray.GetLength(1); y++)
+            {
+                
+            }
+        }
     }
 
 }
